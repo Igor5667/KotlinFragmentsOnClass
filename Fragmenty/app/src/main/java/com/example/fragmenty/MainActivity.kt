511b8434482365486e08e5447dd00a7a2d7ede4e
igor <igor@igor.com>
2024-03-27 13:25:14 +0100

    first

---
+++ Fragmenty/app/src/main/java/com/example/fragmenty/MainActivity.kt
@@ -0,0 +1,58 @@
+package com.example.fragmenty
+
+import android.os.Bundle
+import android.os.PersistableBundle
+import android.view.Menu
+import android.view.MenuItem
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.fragment.app.Fragment
+import com.example.fragmenty.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(binding.root)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        setSupportActionBar(binding.myToolbar)
+        if(savedInstanceState==null){
+            supportFragmentManager
+                .beginTransaction()
+                .replace(R.id.welcomeFragmentContainer, WelcomeFragment())
+                .commit()
+        }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.toolbar_menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when(item.itemId){
+            R.id.airplane -> replaceFragment(AirplaneFragment())
+            R.id.home -> replaceFragment(WelcomeFragment())
+            R.id.inwalider -> replaceFragment(InwaliderFragment())
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+    private fun replaceFragment(fragment: Fragment){
+        supportFragmentManager
+            .beginTransaction()
+            .replace(R.id.welcomeFragmentContainer, fragment)
+            .commit()
+    }
+}
